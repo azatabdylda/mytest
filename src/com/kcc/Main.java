a498eff4faf64024c19d0ff28e0095db6a48761e
azat <azatabdylda@gmail.com>
2019-06-04 00:13:04 -0500

    migrate

--- src/com/kcc/Main.java
+++ src/com/kcc/Main.java
@@ -2,7 +2,36 @@ package com.kcc;
 
 public class Main {
 
+    public Main() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
