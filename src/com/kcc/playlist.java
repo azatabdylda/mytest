3fb06b4105982bbccfcb9ee70735849c2bed9e5a
azatabdylda <51336399+azatabdylda@users.noreply.github.com>
2019-10-15 17:29:48 -0500

    eki
    dcascascas

--- src/com/kcc/playlist.java
+++ src/com/kcc/playlist.java
@@ -6,11 +6,6 @@ public class playlist {
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
